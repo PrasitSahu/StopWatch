6cdd394aa9fb0ecd0a8219d0ce0fb5f116ca7eaa
PrasitSahu <prasitsahu4@gmail.com>
2022-09-03 01:00:23 +0530

    added build

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import registerServiceWorker from "./serviceWorker";
+// import registerServiceWorker from "./serviceWorker";
+import * as registerServiceWorker from "./registerServiceWorker";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -17,4 +18,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-registerServiceWorker();
+registerServiceWorker.register();
